3c3bc17c6ac8716abdc12b2d4c023b64e24e18fd
SebastianIonel <ionel.sebastian01@yahoo.com>
2023-05-16 13:38:51 +0300

    update main

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +17,38 @@ int main(int argc, char *argv[])
         printf("syntax: ./schedule dd/mm/yyyy\n");
         return 1;
     }
+
+    // Afisare obiective AZI
+    // sterge date mai vechi
+    // pentru PATH => creare directory
+    char *command = NULL;
+    command = malloc(300 * sizeof(char));
+    if (command == NULL) {
+        return 1;
+    }
+    
+    while (1) {
+        fgets(command, 300, stdin);
+        // printf("%ld\n", strlen(command));
+        // fputs(command, stdout);
+        if (command[0] == 'E') {
+            break;
+        }
+
+    }
+    
+    
+    
+    
+    
+    
+    // FILE *f = fopen("text", "wt");
+    // if (f == NULL) {
+    //     printf("NU EXISTA\n");
+    // }
+    
+    // mkdir("directory", 0700);
+
 
     return 0;
 }
