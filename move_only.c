4e302ae9a8bded0fb29ee4d577c2721229cafe10
Student USO VM User <student@stud.acs.upb.ro>
2023-05-23 16:54:48 +0300

    add

---
+++ move_only.c
@@ -0,0 +1,57 @@
+#include "functions.h"
+
+void MOVE_ONLY(char *previous_data, char *next_data, int n)
+{
+	previous_data[2] = '.';
+	previous_data[5] = '.';
+	next_data[2] = '.';
+	next_data[5] = '.';
+	char previous_directory[20], next_directory[20];
+	strcpy (previous_directory, "directory/");
+	strcat (previous_directory, previous_data);
+	strcpy (next_directory, "directory/");
+	strcat (next_directory, next_data);
+	FILE *in = fopen(previous_directory, "rt");
+	if (in == NULL) {
+		printf("In data %s\nNIMIC PROGRAMAT\n", data);
+		return;
+	}
+	int i = 0, j = 0, exista = 0;
+	char *activity = malloc(300 * sizeof(char));
+	char **V = NULL;
+	V = malloc(100 * sizeof(char));
+	for (i = 0; i < 100; i++) {
+		V[i] = malloc(300 * sizeof(char *));
+	} 
+	while (fgets(activity, 300, in) && i < n) {
+		strcpy(V[i], "A");
+		strcat(V[i], activity);
+		strcat(V[i], "A");
+		if (i == n - 1) {
+			ADD(next_directory, V[i]);
+		}
+		i ++;
+	}
+	if (exista == 0) {
+		printf("Nu exista activitatea %d in ziua %s", n, data);
+		for(i = 0; i < 100; i++) {
+			free(V[i]);
+		}
+		free(V);
+		free(activity);
+		fclose(in);
+		return;
+	}
+	CLEAR(data);
+	for(j = 0; j <= i; j++) {
+		if (j != n - 1)  {
+			ADD(data, V[j]);
+		}
+	}
+	for(i = 0; i < 100; i++) {
+		free(V[i]);
+	}
+	free(V);
+	free(activity);
+	fclose(in);
+}
